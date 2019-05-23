aba2262af9b98d2c9e1788e705fc909bff24e3f7
Xetera <mythologyali@gmail.com>
2019-05-23 11:03:28 -0700

    dockerized hoshi

--- hoshi.js
+++ hoshi.js
@@ -1,6 +1,21 @@
 require('./src/util/Extensions');
 
-const config = require('./config.json');
+const {
+  HOSHI_TOKEN,
+  HOSHI_OWNER,
+  HOSHI_DATABASE_URL,
+  HOSHI_SENTRY_KEY,
+  HOSHI_SELFSTAR_WARNING_TIMEOUT
+} = process.env;
+
+const config = {
+  token: HOSHI_TOKEN,
+  owner: HOSHI_OWNER,
+  dbURL: HOSHI_DATABASE_URL,
+  sentryKey: HOSHI_SENTRY_KEY,
+  selfstarWarningTimeout: HOSHI_SELFSTAR_WARNING_TIMEOUT
+};
+
 const HoshiClient = require('./src/struct/HoshiClient');
 const Logger = require('./src/util/Logger');
 const Raven = require('raven');
@@ -8,17 +23,17 @@ const Raven = require('raven');
 const client = new HoshiClient(config);
 
 if (config.sentryKey) {
-	Raven.config(config.sentryKey).install();
+  Raven.config(config.sentryKey).install();
 }
 
 client.on('disconnect', () => Logger.warn('Connection lost...'))
-	.on('reconnect', () => Logger.info('Attempting to reconnect...'))
-	.on('error', err => Logger.error(err))
-	.on('warn', info => Logger.warn(info));
+    .on('reconnect', () => Logger.info('Attempting to reconnect...'))
+    .on('error', err => Logger.error(err))
+    .on('warn', info => Logger.warn(info));
 
 client.start();
 
 process.on('unhandledRejection', err => {
-	Logger.error('An unhandled promise rejection occured');
-	Logger.stacktrace(err);
+  Logger.error('An unhandled promise rejection occured');
+  Logger.stacktrace(err);
 });
--- src/struct/Database.js
+++ src/struct/Database.js
@@ -1,38 +1,42 @@
-const { dbURL } = require('../../config.json');
+const dbURL = process.env.HOSHI_DATABASE_URL
 const Logger = require('../util/Logger');
 const path = require('path');
 const readdir = require('util').promisify(require('fs').readdir);
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(dbURL, { logging: false });
+const db = new Sequelize(dbURL, {logging: false});
 
 class Database {
-	static get db() {
-		return db;
-	}
+  static get db() {
+    return db;
+  }
 
-	static async authenticate() {
-		try {
-			await db.authenticate();
-			Logger.info('Connection to database has been established successfully.', { tag: 'Postgres' });
-			await this.loadModels(path.join(__dirname, '..', 'models'));
-		} catch (err) {
-			Logger.error('Unable to connect to the database:', { tag: 'Postgres' });
-			Logger.stacktrace(err, { tag: 'Postgres' });
-			Logger.info('Attempting to connect again in 5 seconds...', { tag: 'Postgres' });
-			setTimeout(this.authenticate, 5000);
-		}
-	}
+  static async authenticate() {
+    try {
+      await db.authenticate();
+      Logger.info(
+          'Connection to database has been established successfully.',
+          {tag: 'Postgres'});
+      await this.loadModels(path.join(__dirname, '..', 'models'));
+    } catch (err) {
+      Logger.error('Unable to connect to the database:', {tag: 'Postgres'});
+      Logger.stacktrace(err, {tag: 'Postgres'});
+      Logger.info(
+          'Attempting to connect again in 5 seconds...', {tag: 'Postgres'});
+      setTimeout(this.authenticate, 5000);
+    }
+  }
 
-	static async loadModels(modelsPath) {
-		const files = await readdir(modelsPath);
+  static async loadModels(modelsPath) {
+    const files = await readdir(modelsPath);
 
-		for (const file of files) {
-			const filePath = path.join(modelsPath, file);
-			if (!filePath.endsWith('.js')) continue;
-			await require(filePath).sync({ alter: true }); // eslint-disable-line no-await-in-loop
-		}
-	}
+    for (const file of files) {
+      const filePath = path.join(modelsPath, file);
+      if (!filePath.endsWith('.js')) continue;
+      await require(filePath).sync(
+          {alter: true});  // eslint-disable-line no-await-in-loop
+    }
+  }
 }
 
 module.exports = Database;
